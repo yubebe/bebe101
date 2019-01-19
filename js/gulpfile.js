6582e24aa8cefde1ccf22df2196ccdbc825a79fa
namgyuyoo <jkafka12@gmail.com>
2019-01-19 17:04:39 +0900

    namgyu eslint settings

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -1,8 +1,16 @@
 const gulp = require('gulp');
-// 걸프 의존성을 여기 씁니다.
+const babel =  require('gulp-babel');
+
 
 gulp.task('default',function() {
-	//걸프작업을 여기에 씁니다.
+	//노드 소스
+	gulp.src("es6/**/*.js")
+	.pipe(babel())
+	.pipe(gulp.dest("dist"));
+	//브라우저소스
+	gulp.src("public/es6/**/*.js")
+	.pipe(babel())
+	.pipe(gulp.dest("public/dist"));
 
 });
 
